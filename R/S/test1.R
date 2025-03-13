b5d4ff9941175995a618fd9bcdf22f3c3277406a
Martinez Araneda <martie@uiowa.edu>
2025-03-13 15:26:53 -0500

    deleted line

--- R/S/test1.R
+++ R/S/test1.R
@@ -59,8 +59,6 @@ dCsdt <- - f * kdf * Cs - (1 - f) * kds * Cs + ka * Cw
 dCwdt <- - ka * Cw + f * kdf * Cs + (1 - f) * kds * Cs -
   kaw.o * Aaw / Vw * (Cw - Ca / Kaw.t) - 
   ko * Af * L / Vw * (Cw - Cf / Kf) # [ng/L]
-
-
 dCfdt <- ko * Af / Vf * (Cw - Cf / Kf) # Cw = [ng/L], Cf = [ng/L]
 dCadt <- kaw.o * Aaw / Va * (Cw - Ca / Kaw.t) -
   ro * Apuf / Va * (Ca - Cpuf / Kpuf) # Ca = [ng/L]
