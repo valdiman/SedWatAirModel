789d4c3b18d346487ba0ee4db0587a8b42e0c014
Martinez Araneda <martie@uiowa.edu>
2026-02-25 09:46:19 -0600

    updated version.

--- Subfolders.R
+++ Subfolders.R
@@ -1,8 +1,8 @@
 ## Scripts to create subfolders within the current project directory
 
 dir.create("R")
-dir.create("R/NS")
-dir.create("R/S")
+dir.create("R/NS") # non-shaked
+dir.create("R/GS") # gentle shaked
 dir.create("Data")
 dir.create("Output")
 dir.create("Output/Data")
